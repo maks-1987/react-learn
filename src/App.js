6f4e295670468ddc2c942fc58622368b4dbeac08
MaxD <max.moderation@gmail.com>
2022-06-08 22:08:45 +0300

    feat: add sorting by select-item

--- src/App.js
+++ src/App.js
@@ -1,42 +1,50 @@
 import './styles/App.css';
 import Counter from './components/Counter';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 
 function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: 'JavaScript', body: 'JavaScript - is program lang' },
-    { id: 2, title: 'JavaScript', body: 'JavaScript - is program lang' },
+    { id: 2, title: 'JavaScript2', body: 'JavaScript3 - is program lang' },
+    { id: 3, title: 'JavaScript3', body: 'JavaScript2 - is program lang' },
   ]);
 
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  // const bodyInputRef = useRef();
+  const [selectedSort, setSelectedSort] = useState('');
 
-  const addNewPost = (e) => {
-    e.preventDefault();
-    // console.log(bodyInputRef.current.value);
-    const newPost = {
-      title,
-      body
-    }
-    console.log(newPost)
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  };
+  // получает post из дочернего компонента
+  const removePost = (post) => {
+    setPosts(posts.filter((item) => item.id !== post.id));
+  };
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+    // console.log([sort]);
   };
 
   return (
     <div className="App">
       <Counter />
-      <form className="form-posts">
-        {/* управляемій компонент */}
-        <MyInput value={title} onChange={(e) => setTitle(e.target.value)} type="text" placeholder="post name" />
-        <MyInput value={body} onChange={(e) => setBody(e.target.value)} type="text" placeholder="post name" />
-        {/* неуправляемый/неконтролируемый компонент */}
-        {/* <MyInput ref={bodyInputRef} type="text" placeholder="post descr" /> */}
-        <MyButton onClick={addNewPost}>Edit post</MyButton>
-      </form>
-      <PostList posts={posts} title="Список постов 2" />
+      <PostForm create={createPost} />
+      <hr style={{ margin: '10px 0px' }} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Sorting by"
+          options={[
+            { value: 'title', name: 'By name' },
+            { value: 'body', name: 'By description' },
+          ]}
+        />
+      </div>
+      {posts.length ? <PostList remove={removePost} posts={posts} title="Список постов 2" /> : <h1 style={{ textAlign: 'center' }}>Posts does not!</h1>}
     </div>
   );
 }
--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const MySelect = ({ options, defaultValue }) => {
+const MySelect = ({ options, defaultValue, value, onChange }) => {
     return (
-        <select>
-            <option disabled value="">{defaultValue}</option>
+        <select value={value} onChange={event => onChange(event.target.value)}>
+            <option disabled >{defaultValue}</option>
             {options.map(option =>
-                <option value={option.value}>
+                <option key={option.value} value={option.value}>
                     {option.name}
                 </option>
             )}
