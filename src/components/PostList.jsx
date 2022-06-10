c634c06adb3262c177e16c4e82ef02ea6ca674f0
MaxD <max.moderation@gmail.com>
2022-06-10 13:41:29 +0300

    refactor: clear dir from other files

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,19 +1,33 @@
 import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import PostItem from "./PostItem";
 
 const PostList = ({ posts, title, remove }) => {
 
-    if(!posts.length) {
-        <h1 style={{ textAlign: 'center' }}>Posts does not!</h1>
+    if (posts.length === 0) {
+        return (
+            <h1 style={{ textAlign: 'center' }}>Posts does not!</h1>
+        )
     }
 
     return (
         <div>
             <h1 style={{ textAlign: 'center' }}>{title}</h1>
-            {posts.map((item, index) => (
-                <PostItem remove={remove} number={index + 1} post={item} key={item.id} />
-            ))}
-
+            <TransitionGroup>
+                {posts.map((item, index) =>
+                    <CSSTransition
+                        key={item.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                        <PostItem
+                            remove={remove}
+                            number={index + 1}
+                            post={item}
+                            key={item.id} />
+                     </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
